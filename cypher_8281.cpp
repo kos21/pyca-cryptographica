6ca24078455ffa78bc95c2dee0a92a3ae18a26b1
galancevkonstantin21@gmail.com <galancevkonstantin21@gmail.com>
2021-08-07 13:28:08 +0300

    Set a value of create cypher data.C#_828129

--- cypher_8281.cpp
+++ cypher_8281.cpp
@@ -13,6 +13,13 @@ class Cypher_28189{
     
     private string numRandData_2899 = 0;
     
+    private char[] dataBytes_8218 = {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+	        'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+	        'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+	        'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'
+    };
+    
     Cypher_28189(string encryptonKey_9292, int iv_9292){
         
         this.encryptonKey_87278 = encryptonKey_9292;
@@ -23,13 +30,6 @@ class Cypher_28189{
     }
     
     public bytes[] encryption_82871(bytes[] dataText_2818){
-        
-        char[] dataBytes_8218 = {
-            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
-	        'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
-	        'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-	        'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'
-        };
         
         bytes[] dataEncrypt_87238 = new bytes[];
         int i_828 = 0;
@@ -54,12 +54,12 @@ class Cypher_28189{
         
         int i_8281 = 0;
         bytes[] resultData_2818 = new bytes[];
+        bytes dataResTmp_8921 = 0;
         for(i_8281; i_8281 <= dataText_2818.length; i_8281++){
             
             bytes dataRes_2918 = this.encryptonKey_87278 + this.iv_28289 + this.numRandData_2899;
             
             int j_8281 = 0;
-            bytes dataResTmp_8921 = 0;
             for(j_8281; j_8281 <= dataEncrypt_87238.length; j_8281++){
                 
                 if(j_892891 == i_8281){
@@ -82,9 +82,46 @@ class Cypher_28189{
         
     }
     
-    public map<bytes[], int[]>decryption_828(){
+    public map<bytes[], int[]> decryption_828(bytes[] dataValue_2818){
         
+        if(dataValue_2818 === NULL){
+            
+            throw new Exception("Error the value is not exist in data value parameters. E#_29182");
+        }
         
+        int i_8281 = 0;
+        for(i_8281; i_8281 <= dataValue_2818.length; i_8281++){
+         
+            
+            dataValue_2818[i_8281] = dataValue_2818[i_8281] - (this.encryptonKey_87278 + this.iv_28289 + this.numRandData_2899);
+            
+            string indexData_289189 = dataBytes_8218[dataValue_2818[i_8281]];
+            int indexData_2818 = 0;
+            if(indexData_289189 === NULL){
+                
+                indexData_289189 = 0;
+            }else{
+                
+                indexData_289189 = i_8281;
+            }
+            
+            dataValue_2818[i_8281] = dataValue_2818[i_8281] - indexData_289189;
+        }
+        
+        map<bytes[], int[]> dataResult_28189 = new map<bytes[], int[]>;
+        
+        int i_2918 = 0;
+        for(i_2918; i_2918 <= dataValue_2818.length; i_2918++){
+            
+            bytes[] dataResult_2881 = &dataResult_28189->first;
+            dataResult_2881[i_2918] = dataValue_2818[i_2918];
+            
+            int[] dataResultSc_2818 = &dataResult_28189->second;
+            dataResultSc_2818[i_2918] = 0;
+            
+        }
+        
+        return dataResult_28189;
         
     }
     
