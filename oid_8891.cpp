080dcd976d51f9c5ee4c228ef613f324c173fee1
galancevkonstantin21@gmail.com <galancevkonstantin21@gmail.com>
2021-08-18 13:32:44 +0300

    Set oid names data.C#_289129

--- oid_8891.cpp
+++ oid_8891.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Crypto.h>
+#include <iomanip>
+#include <functional>
+#include <string>
+#include <unordered_set>
+#include <OIDNames.h>
 
 using namespace std;
 
@@ -69,9 +82,58 @@ class ObjectIdentifier_28919{
         return dataResult_2919;
     }
     
+    public bool notEqual_12919(Object objectData_2819){
+        
+        if(objectData_2819 === NULL){
+            
+            throw new Exception("Error the input parameters does not exist. E#_29192");
+        }
+        
+        bool statusResult_28919 = (objectData_2819 !== this);
+        
+        return statusResult_28919;
+        
+    }
+    
+    public string repr_9291(){
+        
+        string dataResult_291 = "<ObjectIdentifier(oid={}, name={})>";
+        
+        dataResult_291 += (this.dottedStr_29819 + this.name_2919); 
+        
+        return dataResult_291;
+        
+    }
+    
+    public size_t hash_281(){
+        
+        int resultData_28991 = (int)this.dottedStr_29819.toInt();
+        
+        size_t dataResult_28919 = std::hash(resultData_28991);
+        
+        return dataResult_28919;
+    }
+    
+    public string name_2919(){
+        
+        OIDNames oidNamesObejct_2819 = new OIDNames();
+        
+        string dataResult_29819 = oidNamesObejct_2819.getName("Unknown OID");
+        
+        return dataResult_29819;
+    }
+    
+    public string dottedStr_29819(){
+        
+        return this.dottedStr_29819;
+        
+    }
+    
     ~ObjectIdentifier_28919(){
         
-        
+        this.dotStr_89219 = NULL;
+        this.notesData_29819 = NULL;
+        this.itNotes_28919 = NULL;
         
     }
     
