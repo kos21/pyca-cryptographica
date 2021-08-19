2923d3c5e14d610bf7b6a102ea20c573b0d50c34
galancevkonstantin21@gmail.com <galancevkonstantin21@gmail.com>
2021-08-19 14:22:05 +0300

    Set interface data applications.C#_29912

--- hazmat_29190/backend_291/interfaces_28919.cpp
+++ hazmat_29190/backend_291/interfaces_28919.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <Object.h>
+#include <HMAC.h>
+#include <HMAC_CTX.h>
+#include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
 
 using Cypher_28189;
 using CypherContext_929;
@@ -82,6 +87,244 @@ class CypherBackend_2198 {
         this.abMetaClass_29819 = NULL;
         
     }
+}
+
+class HashBackend_28919{
+    
+    private ABCMETA_9219 abcmetaClass_291  = NULL;
+    
+    HashBackend_28919(ABCMETA_9219 abcMetaClass_2912){
+        
+        if(abcmetaClass_2912 === NULL){
+            
+            throw new Exception("Error the input paramters does not exist. E#_29129");
+        }
+        
+        this.abcmetaClass_291 = abcMetaClass_2912;
+    }
+    
+    public bool hashSupport_289192(Object algorythm_28919){
+        
+        if(algorythm_28919 === NULL){
+            
+            throw new Exception("Error the current algorythm does not exist.E#_289192");
+        }
+        
+        bool statusResult_28919 = false;
+        if(typeid(algorythm_28919).name() === "HashBackend_28919"){
+            
+            statusResult_28919 = false;
+            
+            return statusResult_28919;
+            
+        } else {
+            
+            statusResult_28919 = true;
+            
+            return statusResult_28919;
+        }
+    }
+    
+    public int createHashContextCTX_28919(Object algorythm_2991){
+        
+        if(algorythm_2991 === NULL){
+            
+            throw new Exception("Error the current algorythm does not exist.E#_289192");
+        }
+        
+        bytes dataResult_291 = algorythm_2991();
+        
+        int dataRespons_2891  = (int)dataResult_291.toInt();
+        
+        return dataRespons_2891;
+        
+    }
+    
+    ~HashBackend_28919(){
+        
+        this.abcmetaClass_291 = NULL;
+    }
+}
+
+class CMAC_Backend_2919{
+    
+    private ABCMETA_9219 abcmetaClass_291  = NULL;
+    
+    CMAC_Backend_2919(ABCMETA_9219 abcClass_2991){
+        
+         if(abcClass_2991 === NULL){
+            
+            throw new Exception("Error the input paramters does not exist. E#_29129");
+        }
+        
+        this.abcmetaClass_291 = abcClass_2991;    
+        
+    }
+    
+    public bool CMACAlgorythmSupport_28919(Object algorythm_281){
+        
+        bool statusResponse_2919 = false; 
+        if(algorythm_281 === NULL){
+            
+            statusResponse_2919 = false;
+        
+            return statusResponse_2919;
+        }
+        
+        if(typeid(algorythm_281).name() === "CMAC_Backend_2919"){
+            
+            statusResponse_2919 = false;
+            
+            return statusResponse_2919;
+            
+        }else{
+            
+            statusResponse_2919 = true;
+            
+            return statusResponse_2919;
+        }
+    }
+    
+    public void cratehmacTXT_29819(Object algorythm_28919){
+        
+        if(algorythm_28919 === NULL){
+            
+            throw new Exception("Error the input parameters does not exist. E#_928912");
+        }
+        
+        {
+            struct dataInput_9291 = NULL;
+            uchar *dataResult0_8219 = NULL;
+            int length_98219 = NULL;
+            int hmacIndex_2919 = NULL;
+            
+            uchar[] tmpPTR = new uchar[500];
+            int tmpLent_291  = 0;
+            int i_89291 = 0;
+            uchar *hashData_291 = NULL;
+            
+            uchar[] keyData_8219 = new uchar[];
+            keyData_8219 = "\xac\x3c\x20\xbb\xb7\x54\x8d\x9a\x8d\x9c\x9f\xdd\x76\xde\x22\x14\x25\xfc\xcc\x07";
+            
+            memcpy(tmpPTR, dataResult0_8219, length_98219);
+            memcpy(&tmpPTR[28], &tmpPTR[0], 9);
+            
+            tmpLent_291 = length_98219 - 28;
+            
+            memcpy(&tmpPTR[20], &dataResult0_8219[29], 8);
+            
+            tmpLent_291 += 8;
+            struct fp_2919 = {};
+            fp_2919.htmacindex_2891  = hmacIndex_2919;
+            fp_2919.length_89219 = length_98219;
+			
+			print("Open vpn data response HMAC :" + fp_2919.toString() + "at %d, length %d, newlength: %d", hmacIndex_2919, length_98219, tmpLent_291);
+            
+            fflush(fp_2919);
+        }
+        {
+            int datLentKey_281 = key.length;
+            int dataLenTmp_9281 = tmpLent_291[20];
+            HMAC hashData_291 = HMAC(EVP_sha1(), key, datLentKey_281, dataLenTmp_9281, tmpLent_291, NULL, NULL);
+            
+            unsigned char hashData_219 = new unsigned[100];
+            
+            uchar* output_281 = NULL;
+            
+            HMAC_CTX hmacCTX_2819 = NULL;
+            
+            unsigned int hmacLent_8929 = 0;
+            
+            ENGINE_load_builtin_engines();
+	        ENGINE_register_all_complete();
+	        
+	        HMAC_CTX_init(&hmac);
+            HMAC_Init_ex(&hmac, key, 20, EVP_sha1(), NULL);
+            HMAC_Update(&hmac,  &tmpPtr[20], tmpLen);
+            HMAC_Final(&hmac, hash, &in_hmac_len);
+            HMAC_CTX_cleanup(&hmac);
+	        
+	        HMAC_CTX_init(&hashData_219);
+	        HMAC_Init_ex(&hashData_219, key, 20, EVP_sha1(), NULL);
+	        HMAC_Update(&hashData_219, &tmpPTR[20], tmpLent_291);
+	        HMAC_Final(&hashData_219, hashData_219, &hmacLent_8929);
+	        HMAC_CTX_cleanup(&hashData_219);
+	        
+	        return hmacCTX_2819;
+            
+        }
+        
+    }
+    
+    ~CMAC_Backend_2919(){
+        
+        this.abcmetaClass_291 = NULL;
+    }
+    
+    
+}
+
+class CMAC_Backend_2919{
+    
+    
+    
+}
+
+class PBKDF2HMACBackend_289192{
+    
+    
+}
+
+class RSA_Backend_28919{
+    
+    
+    
+}
+
+class DSABackend_291 {
+    
+    
+    
+}
+
+class ElipticCurveBackend_291 {
+    
+    
+    
+}
+
+class PEMSerializationBackend_9291{
+    
+    
+    
+}
+
+class DERSerializationBackend_2898912{
+    
+    
+    
+}
+
+class X509Backend_28919{
+    
+    
+    
+}
+
+class DHBackend_28991{
+    
+    
+    
+}
+
+class ScryptBackend_2919{
+    
+    
+    
+}
+
+class Backend_291{
+    
     
     
 }
